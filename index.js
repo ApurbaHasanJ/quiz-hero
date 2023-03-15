adb46559d561289ecb567766dd1dc91bd45edd44
unknown <apurbahasanj@gmail.com>
2023-03-15 23:42:09 +0600

    Fixed.. 6 questions are now displayed

--- index.js
+++ index.js
@@ -61,6 +61,7 @@ const displayQuiz = (data) => {
   }
 
   data.forEach((quiz, i) => {
+    console.log(i)
     quizContainer.innerHTML += `<div class="m-3 py-3 px-4 shadow-sm rounded">
   <div class="flex items-center">
     <div class="h-8 w-8 bg-green-300 rounded-full flex justify-center items-center text-green-800 mr-3">
@@ -76,7 +77,7 @@ const displayQuiz = (data) => {
 };
 
 // EventListener for quiz submit button
-document.querySelector("#submit").addEventlistener("click", () => {
+document.querySelector("#submit").addEventListener("click", () => {
   if (answers.length < 6) {
     return;
   }
